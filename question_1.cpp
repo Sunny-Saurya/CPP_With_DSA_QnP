d9da81308a7c4c511050e7f3b1b622d97bd6faf8
Sunny_Saurya <sunnysaurya9955@gmail.com>
2024-06-03 12:06:15 +0530

    pattern printing 1

--- question_1.cpp
+++ question_1.cpp
@@ -1,1 +1,26 @@
-// write program of hello world
+/*
+1 2 3 4 
+1 2 3 4
+1 2 3 4 
+1 2 3 4
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the number of rows and columns: "<<endl;
+    cin>>n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for(int j = 1; j<=n; j++)
+        {
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+    
+}
