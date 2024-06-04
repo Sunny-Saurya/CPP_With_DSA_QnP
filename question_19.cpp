dd3ab18b224d0f513aae3bfad2795b2437735050
Sunny_Saurya <sunnysaurya9955@gmail.com>
2024-06-04 22:04:48 +0530

    pattern printing 19

---
+++ question_19.cpp
@@ -0,0 +1,31 @@
+// Question no. 2 of printing shape;
+/*#include<iostream>
+using namespace std;
+
+int main(){
+    int n,i,j,b;
+    cout<<"Enter the number of rows : ";
+    cin>>n;
+
+    i = 1;
+    while(i<=n)
+    {
+        b = 1;
+        while(b<=n-i)
+        {
+            cout<<" ";
+            b++;
+        }
+        j = 1;
+        while(j<=i)
+        {
+            cout<<"*";
+            j++;
+        }
+        cout<<"\n";
+        i++;
+    }
+    
+    return 0;
+}*/
+
