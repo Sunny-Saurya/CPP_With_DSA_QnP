9f7909ab635f2e5a6b3e0ee8776b22c79a9fa453
Sunny_Saurya <sunnysaurya9955@gmail.com>
2024-06-04 22:04:05 +0530

    pattern printing 18

---
+++ question_18.cpp
@@ -0,0 +1,23 @@
+//************By using For loop**************
+/*#include<iostream>
+using namespace std;
+
+int main(){
+    int n,j,i;
+    cout<<"Enter the number of rows : ";
+    cin>>n;
+
+    for(i = 1; i<=n; i++)
+    {
+        for(j = 1; j<=i; j++)
+        {
+            cout<<'*';
+
+        }
+        cout<<"\n";
+    }
+    return 0;
+}*/
+
+
+
