c97958162497f966181d33af83d7d0f7f5b7963f
Sunny_Saurya <sunnysaurya9955@gmail.com>
2024-06-12 20:20:08 +0530

    intersection element of two array

---
+++ question_32.cpp
@@ -0,0 +1,66 @@
+// FIND THE INTERSECTION BETWEEN TWO ARRAY.
+
+#include<iostream>
+using namespace std;
+
+int intersectionArray(int arr[], int brr[], int n, int m)
+{
+    cout << "The intersection between these two array is : " << endl;
+    
+    for(int i = 0; i < n; i++)
+    {
+        
+        
+            if(arr[i] == brr[i])
+            {
+                cout << arr[i] << " ";
+            }
+       
+    }
+}
+
+int main()
+{
+    cout << "First array is : " << endl;
+    int n;
+    cout <<"Enter size of an array : " << " ";
+    cin >> n;
+
+    int arr[100];
+    
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << endl;
+
+    for(int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    cout << "Second array is : " << endl;
+
+    int m;
+    cout <<"Enter size of second array : " << " ";
+    cin >> m;
+
+    int brr[100];
+
+    for(int j = 0; j < m; j++)
+    {
+        cin >> brr[j];
+    }
+
+    cout << endl;
+
+    for(int j = 0; j < m; j++)
+    {
+        cout << brr[j] << " ";
+    }
+
+    intersectionArray(arr, brr, n , m);
+
+}
