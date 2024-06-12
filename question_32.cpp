b3939fae197325e4015de7fe6273cf8f5bd39916
Sunny_Saurya <sunnysaurya9955@gmail.com>
2024-06-12 20:25:15 +0530

    interesection element

--- question_32.cpp
+++ question_32.cpp
@@ -5,17 +5,23 @@ using namespace std;
 
 int intersectionArray(int arr[], int brr[], int n, int m)
 {
-    cout << "The intersection between these two array is : " << endl;
+    cout<< endl << "The intersection between these two array is : " << endl;
     
+    int flag = 0;
     for(int i = 0; i < n; i++)
+    {      
+        if(arr[i] == brr[i])
+        {
+           cout << arr[i] << " ";
+        }
+        else 
+        {
+            flag = 1;
+        }
+    }
+    if(flag == 1)
     {
-        
-        
-            if(arr[i] == brr[i])
-            {
-                cout << arr[i] << " ";
-            }
-       
+        cout << "No, interesection element ";
     }
 }
 
