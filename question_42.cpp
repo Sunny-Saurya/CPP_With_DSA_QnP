6928b29315378069be2f607432e66be0316debaf
Sunny_Saurya <sunnysaurya9955@gmail.com>
2024-06-16 23:10:44 +0530

    new

--- question_42.cpp
+++ question_42.cpp
@@ -1,3 +1,5 @@
+// Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
 #include <iostream>
 using namespace std;
 
