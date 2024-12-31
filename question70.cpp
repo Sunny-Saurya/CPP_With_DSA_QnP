6fe901dc815100280f0e5a9bdbf19fca4af99a21
Sunny Kumar <sunnysaurya9955@gmail.com>
2024-12-31 21:06:21 +0530

    question 70

---
+++ question70.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int pivotInteger(int n) {
+        
+        int sum = n * (n + 1)/ 2;
+        double a = sqrt(sum);
+
+        if(a - ceil(a) == 0)
+        {
+            return int(a);
+        }
+        else{
+            return -1;
+        }
+
+    }
+};
